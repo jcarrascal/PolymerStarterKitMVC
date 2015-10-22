24dab3b6e07335747796829670e621eb1004f197
Julio César Carrascal Urquijo <jcarrascal@gmail.com>
2015-10-21 22:27:35 -0500

    Dynamic detection of elements

---
+++ PolymerStarterKitLight/PolymerStarterKitLight/Attributes/ElementAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PolymerStarterKitLight.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public sealed class ElementAttribute : Attribute
+    {
+    }
+}
--- PolymerStarterKitLight/PolymerStarterKitLight/Controllers/HomeController.cs
+++ PolymerStarterKitLight/PolymerStarterKitLight/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using PolymerStarterKitLight.Attributes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,6 +13,19 @@ namespace PolymerStarterKitLight.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            return View();
+        }
+
+        // GET: Elements
+        public ActionResult Elements()
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            IEnumerable<Tuple<string, string>> actions = executingAssembly.GetTypes()
+                .Where(type => typeof(Controller).IsAssignableFrom(type))
+                .SelectMany(type => type.GetMethods())
+                .Where(method => method.IsPublic && method.IsDefined(typeof(ElementAttribute)))
+                .Select(method => Tuple.Create(method.Name, method.DeclaringType.Name.Replace("Controller", string.Empty)));
+            this.ViewBag.Actions = actions;
             return View();
         }
     }
